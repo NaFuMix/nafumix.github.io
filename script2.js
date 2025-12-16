f72af854fda22d55780177b3596c78bcf00833f2
NaFuMix <nonthapat.buated@gmail.com>
2025-12-16 16:05:15 +0700

    add script2 certificate

--- script2.js
+++ script2.js
@@ -3,15 +3,21 @@ import { certificate } from "./data.js";
 const container = document.getElementById("certificate")
 
 certificate.forEach(cer => {
-
+    var n = 0
     let div_element = document.createElement("div")
     div_element.setAttribute("class", "certificate-img")
 
     let img_element = document.createElement("img")
     img_element.src = cer.pic_url
-    // img_element.addEventListener("click", () => {
-    //     img_element.setAttribute("class","")
-    // })
+    img_element.addEventListener("click", function() {
+        n += 1
+        if(n == 1){
+            img_element.style.scale = 1.5
+        }else if( n == 2){
+            img_element.style.scale = 1.0
+            n = 0
+        }
+    })
 
     let h2_element = document.createElement("h2")
     h2_element.innerHTML = cer.name
