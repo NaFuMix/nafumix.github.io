15ed86c0ef09f7c5c86160ce857944ec142df7d6
NaFuMix <nonthapat.buated@gmail.com>
2025-12-16 19:31:40 +0700

    update transition scale certificate

--- script2.js
+++ script2.js
@@ -3,18 +3,24 @@ import { certificate } from "./data.js";
 const container = document.getElementById("certificate")
 
 certificate.forEach(cer => {
-    var n = 0
+    let n = 0
+
     let div_element = document.createElement("div")
     div_element.setAttribute("class", "certificate-img")
-
+    
     let img_element = document.createElement("img")
     img_element.src = cer.pic_url
-    img_element.addEventListener("click", function() {
-        n += 1
-        if(n == 1){
-            img_element.style.scale = 1.5
-        }else if( n == 2){
-            img_element.style.scale = 1.0
+    img_element.addEventListener("click", () => {
+        if (n == 0) {
+            img_element.classList.contains("scale")
+            container.querySelectorAll('.scale').forEach(el => el.classList.remove('scale'))
+            if (!img_element.classList.contains("scale")) {
+                img_element.classList.add('scale')
+                h2_element.classList.add('scale')
+            }
+            n += 1
+        } else if (n == 1) {
+            container.querySelectorAll('.scale').forEach(el => el.classList.remove('scale'))
             n = 0
         }
     })
